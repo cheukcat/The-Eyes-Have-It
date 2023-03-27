c434cd2b29ecae0a072f0b3dfee63325dd1b3c77
cuibihao <bihao.cui@hongjingdrive.com>
2023-03-27 15:33:52 +0800

    create gridmap anchor impl, to generate sampling points on 3d space

--- occnet/view_transformers/inverse_matrixVT.py
+++ occnet/view_transformers/inverse_matrixVT.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mmcv.runner import BaseModule, auto_fp16
@@ -7,14 +8,35 @@ from occnet import VIEW_TRANSFORMERS
 @VIEW_TRANSFORMERS.register_module()
 class InverseMatrixVT(BaseModule):
     def __init__(self,
+                 grid_size=[100, 100, 8],
                  x_bound=[-51.2, 51.2],
                  y_bound=[-51.2, 51.2],
-                 z_bound=[-5., 3.]):
+                 z_bound=[-5., 3.],
+                 sampling_rate=4):
         super().__init__()
+        self.grid_size = torch.tensor(grid_size)
         self.x_bound = x_bound
         self.y_bound = y_bound
         self.z_bound = z_bound
+        self.sampling_rate = sampling_rate
+        self.coord = self._create_gridmap_anchor()
 
-    def create_gridmap_anchor(self, grid_size):
-        # cal
-        pass
+    def _create_gridmap_anchor(self):
+        # create a gridmap anchor with shape of (X, Y, Z, sampling_rate**3, 3)
+        grid_size = self.sampling_rate * self.grid_size
+        coord = torch.zeros(grid_size[0], grid_size[1], grid_size[2], 3)
+        x_coord = torch.linspace(self.x_bound[0], self.x_bound[1], grid_size[0])
+        y_coord = torch.linspace(self.y_bound[0], self.y_bound[1], grid_size[1])
+        z_coord = torch.linspace(self.z_bound[0], self.z_bound[1], grid_size[2])
+        ones = torch.ones(grid_size[0], grid_size[1], grid_size[2], 1)
+        coord[:, :, :, 0] = x_coord.reshape(-1, 1, 1)
+        coord[:, :, :, 1] = y_coord.reshape(1, -1, 1)
+        coord[:, :, :, 2] = z_coord.reshape(1, 1, -1)
+        coord = torch.cat([coord, ones], dim=-1)
+        # taking multi sampling points into a single grid
+        new_coord = coord.reshape(self.grid_size[0], self.sampling_rate,
+                                  self.grid_size[1], self.sampling_rate,
+                                  self.grid_size[2], self.sampling_rate, 4).\
+            permute(0, 2, 4, 1, 3, 5, 6).reshape(self.grid_size[0], self.grid_size[1],
+                                                 self.grid_size[2], -1, 4)
+        return new_coord
