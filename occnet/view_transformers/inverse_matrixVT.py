1ed7ce9de23348ead37616b92a1ead826cd17f8f
cuibihao <bihao.cui@hongjingdrive.com>
2023-03-27 17:30:51 +0800

    create ref_points for inverse vt matrix

--- occnet/view_transformers/inverse_matrixVT.py
+++ occnet/view_transformers/inverse_matrixVT.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from mmcv.runner import BaseModule, auto_fp16
+import numpy as np
+from mmcv.runner import BaseModule, force_fp32, auto_fp16
 from occnet import VIEW_TRANSFORMERS
 
 
@@ -40,3 +41,28 @@ class InverseMatrixVT(BaseModule):
             permute(0, 2, 4, 1, 3, 5, 6).reshape(self.grid_size[0], self.grid_size[1],
                                                  self.grid_size[2], -1, 4)
         return new_coord
+
+    @force_fp32(apply_to=('img_metas'))
+    def get_ref_points(self, img_feats, img_metas):
+        B, N, C, H, W = img_feats.shape
+        # lidar2img: (B, N, 4, 4)
+        lidar2img = []
+        for img_meta in img_metas:
+            lidar2img.append(img_meta['lidar2img'])
+        lidar2img = np.asarray(lidar2img)
+        lidar2img = img_feats.new_tensor(lidar2img)
+        # global_coord: (B, N, X * Y * Z, S, 4, 1)
+        global_coord = self.coord.clone()
+        X, Y, Z, S, _ = global_coord.shape
+        global_coord = global_coord.unsqueeze(0).unsqueeze(1)\
+            .view(1, 1, X * Y * Z, S, 4, 1)\
+            .repeat(B, N, 1, 1, 1, 1)
+        # lidar2img: (B, N, X * Y * Z, S, 4, 4)
+        lidar2img = lidar2img.unsqueeze(2).unsqueeze(3)\
+            .repeat(1, 1, X * Y * Z, S, 1, 1)
+        # ref_points: (B, N, X * Y * Z, S, 4)
+        ref_points = torch.matmul(lidar2img.to(torch.float32),
+                                  global_coord.to(torch.float32)).squeeze(-1)
+        return ref_points
+
+
