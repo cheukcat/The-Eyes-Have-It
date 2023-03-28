df23ba7066386439f084cf47c0e765770c1522ed
cuibihao <bihao.cui@hongjingdrive.com>
2023-03-28 17:41:03 +0800

    create vt matrix impl, on cpu checked, on cuda unchecked

--- occnet/view_transformers/inverse_matrixVT.py
+++ occnet/view_transformers/inverse_matrixVT.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 from mmcv.runner import BaseModule, force_fp32, auto_fp16
 from occnet import VIEW_TRANSFORMERS
+from occnet.utils import multi_apply
 
 
 @VIEW_TRANSFORMERS.register_module()
@@ -13,13 +14,15 @@ class InverseMatrixVT(BaseModule):
                  x_bound=[-51.2, 51.2],
                  y_bound=[-51.2, 51.2],
                  z_bound=[-5., 3.],
-                 sampling_rate=4):
+                 sampling_rate=4,
+                 ds_rate=32.):
         super().__init__()
         self.grid_size = torch.tensor(grid_size)
         self.x_bound = x_bound
         self.y_bound = y_bound
         self.z_bound = z_bound
         self.sampling_rate = sampling_rate
+        self.ds_rate = ds_rate
         self.coord = self._create_gridmap_anchor()
 
     def _create_gridmap_anchor(self):
@@ -37,32 +40,60 @@ class InverseMatrixVT(BaseModule):
         # taking multi sampling points into a single grid
         new_coord = coord.reshape(self.grid_size[0], self.sampling_rate,
                                   self.grid_size[1], self.sampling_rate,
-                                  self.grid_size[2], self.sampling_rate, 4).\
+                                  self.grid_size[2], self.sampling_rate, 4). \
             permute(0, 2, 4, 1, 3, 5, 6).reshape(self.grid_size[0], self.grid_size[1],
                                                  self.grid_size[2], -1, 4)
         return new_coord
 
-    @force_fp32(apply_to=('img_metas'))
-    def get_ref_points(self, img_feats, img_metas):
-        B, N, C, H, W = img_feats.shape
-        # lidar2img: (B, N, 4, 4)
-        lidar2img = []
-        for img_meta in img_metas:
-            lidar2img.append(img_meta['lidar2img'])
+    def get_vt_matrix(self, img_feats, img_metas):
+        batch_vt = multi_apply(self._get_vt_matrix_single,
+                               img_feats,
+                               img_metas)
+        return torch.stack(batch_vt[0])
+
+    @force_fp32(apply_to=('img_feat', 'img_meta'))
+    def _get_vt_matrix_single(self, img_feat, img_meta):
+        Nc, C, H, W = img_feat.shape
+        # lidar2img: (Nc, 4, 4)
+        lidar2img = img_meta['lidar2img']
         lidar2img = np.asarray(lidar2img)
-        lidar2img = img_feats.new_tensor(lidar2img)
-        # global_coord: (B, N, X * Y * Z, S, 4, 1)
+        lidar2img = img_feat.new_tensor(lidar2img)
+        img_shape = img_meta['img_shape']
+        # global_coord: (X * Y * Z, Nc, S, 4, 1)
         global_coord = self.coord.clone()
         X, Y, Z, S, _ = global_coord.shape
-        global_coord = global_coord.unsqueeze(0).unsqueeze(1)\
-            .view(1, 1, X * Y * Z, S, 4, 1)\
-            .repeat(B, N, 1, 1, 1, 1)
-        # lidar2img: (B, N, X * Y * Z, S, 4, 4)
-        lidar2img = lidar2img.unsqueeze(2).unsqueeze(3)\
-            .repeat(1, 1, X * Y * Z, S, 1, 1)
-        # ref_points: (B, N, X * Y * Z, S, 4)
+        global_coord = global_coord.view(X * Y * Z, 1, S, 4, 1) \
+            .repeat(1, Nc, 1, 1, 1)
+        # lidar2img: (X * Y * Z, Nc, S, 4, 4)
+        lidar2img = lidar2img.view(1, Nc, 1, 4, 4) \
+            .repeat(X * Y * Z, 1, S, 1, 1)
+        # ref_points: (X * Y * Z, Nc, S, 4), 4: (λW, λH, λ, 1)
         ref_points = torch.matmul(lidar2img.to(torch.float32),
                                   global_coord.to(torch.float32)).squeeze(-1)
-        return ref_points
+        ref_points[..., 0] = ref_points[..., 0] / ref_points[..., 2]
+        ref_points[..., 1] = ref_points[..., 1] / ref_points[..., 2]
+        # remove invalid sampling points
+        invalid_w = torch.logical_or(ref_points[..., 0] < 0.,
+                                     ref_points[..., 0] > (img_shape[0][1] - 1))
+        invalid_h = torch.logical_or(ref_points[..., 1] < 0.,
+                                     ref_points[..., 1] > (img_shape[0][0] - 1))
+        invalid_d = ref_points[..., 2] < 0.
 
-
+        ref_points = torch.div(ref_points[..., :2],
+                               self.ds_rate,
+                               rounding_mode='floor').to(torch.long)
+        cam_index = torch.arange(Nc).unsqueeze(0).unsqueeze(2) \
+            .repeat(X * Y * Z, 1, S).unsqueeze(-1)
+        # ref_points: (X * Y * Z, Nc * S, 3), 3: (W, H, Nc)
+        ref_points = torch.cat([ref_points, cam_index], dim=-1)
+        ref_points[(invalid_w | invalid_h | invalid_d)] = -1
+        ref_points = ref_points.view(X * Y * Z, -1, 3)
+        # ref_points_flatten: (X * Y * Z, Nc * S), 1: H * W * nc + W * h + w
+        ref_points_flatten = ref_points[..., 2] * H * W + \
+                             ref_points[..., 1] * W + ref_points[..., 0]
+        # create vt matrix
+        vt = img_feat.new_zeros(Nc * H * W, X * Y * Z)
+        valid_idx = torch.nonzero(ref_points_flatten > 0)
+        vt[ref_points_flatten[valid_idx[:, 0], valid_idx[:, 1]], valid_idx[:, 0]] = 1
+        vt /= vt.sum(0).clip(min=1)
+        return vt.unsqueeze(0)
