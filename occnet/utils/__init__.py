84aa10e606c3bbd02d815be73f34a51b67ee455b
cuibihao <bihao.cui@hongjingdrive.com>
2023-03-28 14:53:31 +0800

    multi apply

--- occnet/utils/__init__.py
+++ occnet/utils/__init__.py
@@ -2,6 +2,7 @@ from .grid_mask import *
 from .lovasz_losses import *
 from .revise_ckpt import *
 from .metric_util import *
+from .misc import *
 
 __all__ = ['MeanIoU', 'lovasz_softmax', 'Grid', 'GridMask',
-           'revise_ckpt', 'revise_ckpt_2']
+           'revise_ckpt', 'revise_ckpt_2', 'multi_apply']
