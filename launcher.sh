ab6cb39c5d9c73f259c372994b38ec7f63ae5735
cheukcat <bihao.cui@tum.de>
2023-04-07 16:18:11 +0800

    work dir

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,7 @@
 #!bin/sh
 CONFIG=$1
 WORKDIR=$2
-TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
 
 shift 2
 
-python ./tools/train.py --py-config $CONFIG --work-dir $WORKDIR "$@" --timestamp $TIMESTAMP
+python ./tools/train.py --py-config $CONFIG --work-dir $WORKDIR "$@"
