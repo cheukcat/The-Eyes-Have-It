d29ee110d14887147505933e8dd642d3433e8e1a
DeclK <1605224559@qq.com>
2023-04-11 09:45:07 +0000

    organize vis scripts

--- tools/tutorials/vis.py
+++ tools/tutorials/vis.py
@@ -2,75 +2,90 @@ import sys
 import os
 from pathlib import Path
 # TODO: we haven't build the project yet! So add the project to the env path
-sys.path.append(str(Path(__file__).parents[2]))
-print(str(Path(__file__).parents[2]))
+project_dir = str(Path(__file__).parents[2])
+sys.path.append(project_dir)
+os.chdir(project_dir)
 
 import numpy as np
 import shutil
+import pickle
+from tqdm import tqdm
+from mmcv import Config
+from argparse import ArgumentParser
 from tools.visualization.vis_dataset import build_vis_dataset
 from tools.visualization.vis_utils import draw
 
-# set configs
-# set the path to your own
-version = 'v1.0-trainval'
+def Visualiation(args):
+    """ Visualize occupancy scenes with or without prediction results
+    """
+    config = Config.fromfile(args.config)
+    dataset_config = config['dataset_params']
+    val_dataset_config = config['val_data_loader']
+    dataset_config.version = args.version
 
-dataset_config = dict(
-    version = version,
-    ignore_label = 0,
-    fill_label = 17,
-    fixed_volume_space = True,
-    label_mapping = "/works/cbh/The-Eyes-Have-It/configs/_base_/label_mapping/nuscenes-noIgnore.yaml",
-    max_volume_space = [51.2, 51.2, 3],
-    min_volume_space = [-51.2, -51.2, -5],
-)
-dataloader_config = dict(
-    data_path = "/works/cbh/The-Eyes-Have-It/data/nuscenes",
-    imageset = "/works/cbh/The-Eyes-Have-It/data/nuscenes_infos_val.pkl",
-    batch_size = 1,
-    shuffle = True,
-    num_workers = 0,    # debug use
-)
+    grid_size = config.grid_size
+    vis_dataset = build_vis_dataset(dataset_config,
+                                    val_dataset_config,
+                                    grid_size)    
+    # get results
+    if args.results is not None:
+        results = pickle.load(open(args.results, 'rb'))
+    else: results = None
 
-# build visualization dataset
-vis_dataset = build_vis_dataset(dataset_config,
-                                dataloader_config)
+    # create process bar
+    pbar = tqdm(total=len(vis_dataset))
 
-# visualization index
-index = 0
-# get the sample
-batch_data, filelist, scene_meta, timestamp = vis_dataset[index]
-imgs, img_metas, vox_label, grid, pt_label = batch_data
+    for index in range(len(vis_dataset)):
+        # get the sample
+        batch_data, filelist, scene_meta, timestamp = vis_dataset[index]
+        imgs, img_metas, vox_label, grid, pt_label = batch_data
 
-grid_size = [200, 200, 16]
-imgs = np.stack([imgs]) # not used for now
-grid = np.stack([grid])
+        imgs = np.stack([imgs])     # imgs not used for now
+        grid = np.stack([grid])
 
-voxel_origin = dataset_config['min_volume_space']
-voxel_max = dataset_config['max_volume_space']
-resolution = [(e - s) / l for e, s, l in zip(voxel_max, voxel_origin, grid_size)]
+        voxel_origin = dataset_config['min_volume_space']
+        voxel_max = dataset_config['max_volume_space']
+        resolution = [(e - s) / l for e, s, l in
+                      zip(voxel_max, voxel_origin, grid_size)]
 
-save_path = 'vis_output'
+        # create save dir
+        save_path = Path('vis_output')
+        frame_dir = save_path / str(index)
+        frame_dir.mkdir(parents=True, exist_ok=True)
 
-frame_dir = os.path.join(save_path, str(index))
-os.makedirs(frame_dir, exist_ok=True)
+        for filename in filelist:   # imgs not used for now            
+            filename_ = Path(filename).name
+            shutil.copy(filename, frame_dir / filename_)
 
-for filename in filelist:   # not used for now            
-    shutil.copy(filename, os.path.join(frame_dir, os.path.basename(filename)))
+        if results is not None:
+            predict_vox = results[index]
+            predict_pts = None  # pts not used for now
+        else: # create prediction placeholders
+            predict_vox = np.ones(grid_size)
+            predict_pts = None
 
-# create prediction placeholders
-voxels_shape = [256, 256, 32]
-predict_vox = np.ones(voxels_shape)
-predict_pts = None
+        voxel_nums = draw(predict_vox, 
+                          predict_pts,
+                          voxel_origin, 
+                          resolution, 
+                          grid.squeeze(0), 
+                          pt_label.squeeze(-1),
+                          frame_dir,
+                          img_metas['cam_positions'],
+                          img_metas['focal_positions'],
+                          timestamp=timestamp,
+                          offscreen=True,    # save fig
+                          mode=2)
 
-draw(predict_vox, 
-     predict_pts,
-     voxel_origin, 
-     resolution, 
-     grid.squeeze(0), 
-     pt_label.squeeze(-1),
-     frame_dir,
-     img_metas['cam_positions'],
-     img_metas['focal_positions'],
-     timestamp=timestamp,
-     offscreen=True,    # save the fig!
-     mode=2)
+        pbar.set_postfix({'voxel numbers': voxel_nums})
+        pbar.update()
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='visualize the occupancy grid')
+    parser.add_argument('--config', default='configs/vanilla_occ_r50_fpn_1x_nus-trainval.py')
+    parser.add_argument('--version', default='v1.0-trainval', help='v1.0-trainval or v1.0-mini')
+    parser.add_argument('--results', default=None, help='prediction result file')
+    parser.add_argument('--mode', default=2, type=int, help='0: pred occ, 1: pred pts, 2: gt occ')
+
+    args = parser.parse_args()
+    Visualiation(args)
--- tools/visualization/vis_utils.py
+++ tools/visualization/vis_utils.py
@@ -25,6 +25,59 @@ def get_grid_coords(dims, resolution):
 
     return coords_grid
 
+def get_color_palette():
+    return np.array(
+        [
+            [255, 120,  50, 255],       # barrier              orange
+            [255, 192, 203, 255],       # bicycle              pink
+            [255, 255,   0, 255],       # bus                  yellow
+            [  0, 150, 245, 255],       # car                  blue
+            [  0, 255, 255, 255],       # construction_vehicle cyan
+            [255, 127,   0, 255],       # motorcycle           dark orange
+            [255,   0,   0, 255],       # pedestrian           red
+            [255, 240, 150, 255],       # traffic_cone         light yellow
+            [135,  60,   0, 255],       # trailer              brown
+            [160,  32, 240, 255],       # truck                purple                
+            [255,   0, 255, 255],       # driveable_surface    dark pink
+            # [175,   0,  75, 255],       # other_flat           dark red
+            [139, 137, 137, 255],
+            [ 75,   0,  75, 255],       # sidewalk             dard purple
+            [150, 240,  80, 255],       # terrain              light green          
+            [230, 230, 250, 255],       # manmade              white
+            [  0, 175,   0, 255],       # vegetation           green
+            [  0, 255, 127, 255],       # ego car              dark cyan
+            [255,  99,  71, 255],       # ego car
+            [  0, 191, 255, 255]        # ego car
+        ]
+    ).astype(np.uint8)
+
+def draw_simple_car(w, h, z, grid_coords):
+    """ Draw a simple car at the middle
+    """
+    car_vox_range = np.array([
+        [w//2 - 2 - 4, w//2 - 2 + 4],
+        [h//2 - 2 - 4, h//2 - 2 + 4],
+        [z//2 - 2 - 3, z//2 - 2 + 3]
+    ], dtype=np.int)
+    car_x = np.arange(car_vox_range[0, 0], car_vox_range[0, 1])
+    car_y = np.arange(car_vox_range[1, 0], car_vox_range[1, 1])
+    car_z = np.arange(car_vox_range[2, 0], car_vox_range[2, 1])
+    car_xx, car_yy, car_zz = np.meshgrid(car_x, car_y, car_z)
+    car_label = np.zeros([8, 8, 6], dtype=np.int)
+    car_label[:3, :, :2] = 17
+    car_label[3:6, :, :2] = 18
+    car_label[6:, :, :2] = 19
+    car_label[:3, :, 2:4] = 18
+    car_label[3:6, :, 2:4] = 19
+    car_label[6:, :, 2:4] = 17
+    car_label[:3, :, 4:] = 19
+    car_label[3:6, :, 4:] = 17
+    car_label[6:, :, 4:] = 18
+    car_grid = np.array([car_xx.flatten(), car_yy.flatten(), car_zz.flatten()]).T
+    car_indexes = car_grid[:, 0] * h * z + car_grid[:, 1] * z + car_grid[:, 2]
+    grid_coords[car_indexes, 3] = car_label.flatten()
+
+
 def draw(
     voxels,          # semantic occupancy predictions
     pred_pts,        # lidarseg predictions
@@ -51,31 +104,7 @@ def draw(
 
     if mode == 0:
         grid_coords = np.vstack([grid_coords.T, voxels.reshape(-1)]).T
-    
-        # draw a simple car at the middle
-        # car_vox_range = np.array([
-        #     [w//2 - 2 - 4, w//2 - 2 + 4],
-        #     [h//2 - 2 - 4, h//2 - 2 + 4],
-        #     [z//2 - 2 - 3, z//2 - 2 + 3]
-        # ], dtype=np.int)
-        # car_x = np.arange(car_vox_range[0, 0], car_vox_range[0, 1])
-        # car_y = np.arange(car_vox_range[1, 0], car_vox_range[1, 1])
-        # car_z = np.arange(car_vox_range[2, 0], car_vox_range[2, 1])
-        # car_xx, car_yy, car_zz = np.meshgrid(car_x, car_y, car_z)
-        # car_label = np.zeros([8, 8, 6], dtype=np.int)
-        # car_label[:3, :, :2] = 17
-        # car_label[3:6, :, :2] = 18
-        # car_label[6:, :, :2] = 19
-        # car_label[:3, :, 2:4] = 18
-        # car_label[3:6, :, 2:4] = 19
-        # car_label[6:, :, 2:4] = 17
-        # car_label[:3, :, 4:] = 19
-        # car_label[3:6, :, 4:] = 17
-        # car_label[6:, :, 4:] = 18
-        # car_grid = np.array([car_xx.flatten(), car_yy.flatten(), car_zz.flatten()]).T
-        # car_indexes = car_grid[:, 0] * h * z + car_grid[:, 1] * z + car_grid[:, 2]
-        # grid_coords[car_indexes, 3] = car_label.flatten()
-    
+        draw_simple_car(w, h, z, grid_coords)
     elif mode == 1:
         indexes = grid[:, 0] * h * z + grid[:, 1] * z + grid[:, 2]
         indexes, pt_index = np.unique(indexes, return_index=True)
@@ -100,7 +129,6 @@ def draw(
     fov_voxels = fov_grid_coords[
         (fov_grid_coords[:, 3] > 0) & (fov_grid_coords[:, 3] < 20)
     ]
-    print(len(fov_voxels))
     
     figure = mlab.figure(size=(2560, 1440), bgcolor=(1, 1, 1))
     # Draw occupied inside FOV voxels
@@ -118,30 +146,7 @@ def draw(
         vmax=19, # 16
     )
 
-    colors = np.array(
-        [
-            [255, 120,  50, 255],       # barrier              orange
-            [255, 192, 203, 255],       # bicycle              pink
-            [255, 255,   0, 255],       # bus                  yellow
-            [  0, 150, 245, 255],       # car                  blue
-            [  0, 255, 255, 255],       # construction_vehicle cyan
-            [255, 127,   0, 255],       # motorcycle           dark orange
-            [255,   0,   0, 255],       # pedestrian           red
-            [255, 240, 150, 255],       # traffic_cone         light yellow
-            [135,  60,   0, 255],       # trailer              brown
-            [160,  32, 240, 255],       # truck                purple                
-            [255,   0, 255, 255],       # driveable_surface    dark pink
-            # [175,   0,  75, 255],       # other_flat           dark red
-            [139, 137, 137, 255],
-            [ 75,   0,  75, 255],       # sidewalk             dard purple
-            [150, 240,  80, 255],       # terrain              light green          
-            [230, 230, 250, 255],       # manmade              white
-            [  0, 175,   0, 255],       # vegetation           green
-            [  0, 255, 127, 255],       # ego car              dark cyan
-            [255,  99,  71, 255],       # ego car
-            [  0, 191, 255, 255]        # ego car
-        ]
-    ).astype(np.uint8)
+    colors = get_color_palette()
     
     plt_plot_fov.glyph.scale_mode = "scale_by_vector"
     plt_plot_fov.module_manager.scalar_lut_manager.lut.table = colors
@@ -159,6 +164,12 @@ def draw(
         mlab.savefig(str(Path(save_dir) / 'fig.png'))
     else:
         mlab.show()
+    mlab.clf() # clear figure, or the memory will leak
+    mlab.close(figure)
+    mlab.close(all=True)
+    
+    return len(fov_voxels)
+
 
 def draw_occ(
     voxels,          # semantic occupancy predictions
@@ -205,30 +216,7 @@ def draw_occ(
         vmax=19, # 16
     )
 
-    colors = np.array(
-        [
-            [255, 120,  50, 255],       # barrier              orange
-            [255, 192, 203, 255],       # bicycle              pink
-            [255, 255,   0, 255],       # bus                  yellow
-            [  0, 150, 245, 255],       # car                  blue
-            [  0, 255, 255, 255],       # construction_vehicle cyan
-            [255, 127,   0, 255],       # motorcycle           dark orange
-            [255,   0,   0, 255],       # pedestrian           red
-            [255, 240, 150, 255],       # traffic_cone         light yellow
-            [135,  60,   0, 255],       # trailer              brown
-            [160,  32, 240, 255],       # truck                purple                
-            [255,   0, 255, 255],       # driveable_surface    dark pink
-            # [175,   0,  75, 255],       # other_flat           dark red
-            [139, 137, 137, 255],
-            [ 75,   0,  75, 255],       # sidewalk             dard purple
-            [150, 240,  80, 255],       # terrain              light green          
-            [230, 230, 250, 255],       # manmade              white
-            [  0, 175,   0, 255],       # vegetation           green
-            [  0, 255, 127, 255],       # ego car              dark cyan
-            [255,  99,  71, 255],       # ego car
-            [  0, 191, 255, 255]        # ego car
-        ]
-    ).astype(np.uint8)
+    colors = get_color_palette()
     
     plt_plot_fov.glyph.scale_mode = "scale_by_vector"
     plt_plot_fov.module_manager.scalar_lut_manager.lut.table = colors
@@ -246,3 +234,8 @@ def draw_occ(
         mlab.savefig(str(save_name))
     else:
         mlab.show()
+    mlab.clf() # clear figure, or the memory will leak
+    mlab.close(figure)
+    mlab.close(all=True)
+
+    return len(fov_voxels)
